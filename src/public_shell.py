51c0d771e5d0af0114c3ca8a4ad5ac827e802d32
b2gsolinteg <b2gsolinteg@gmail.com>
2026-08-28 14:56:35 -0300

    Aplica versões correta da marca em fundo claro e escuro

--- src/public_shell.py
+++ src/public_shell.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import streamlit as st
 
-from src.brand_assets import APPROVED_LOGO_DATA_URI
+from src.brand_assets import APPROVED_LOGO_DARK_DATA_URI, APPROVED_LOGO_LIGHT_DATA_URI
 from src.public_auth import render_public_auth as _render_public_auth
 from src.public_landing_exact import render_public_landing as _render_public_landing
 
@@ -18,25 +18,25 @@ def _approved_brand_css() -> str:
     .lnx-top .lnx-brand {{
         width:255px!important;
         height:64px!important;
-        background-image:url("{APPROVED_LOGO_DATA_URI}")!important;
+        background-image:url("{APPROVED_LOGO_DARK_DATA_URI}")!important;
         background-repeat:no-repeat!important;
         background-position:left center!important;
-        background-size:255px 64px!important;
+        background-size:253px auto!important;
         flex:0 0 255px!important;
     }}
 
     .lnx-auth-head .lnx-brand {{
         width:300px!important;
-        height:75px!important;
-        background-image:url("{APPROVED_LOGO_DATA_URI}")!important;
+        height:76px!important;
+        background-image:url("{APPROVED_LOGO_LIGHT_DATA_URI}")!important;
         background-repeat:no-repeat!important;
         background-position:left center!important;
-        background-size:300px 75px!important;
+        background-size:296px auto!important;
     }}
 
     @media(max-width:680px) {{
-        .lnx-top .lnx-brand {{ width:190px!important;height:48px!important;background-size:190px 48px!important;flex-basis:190px!important; }}
-        .lnx-auth-head .lnx-brand {{ width:230px!important;height:58px!important;background-size:230px 58px!important; }}
+        .lnx-top .lnx-brand {{ width:190px!important;height:48px!important;background-size:188px auto!important;flex-basis:190px!important; }}
+        .lnx-auth-head .lnx-brand {{ width:230px!important;height:59px!important;background-size:226px auto!important; }}
     }}
     </style>
     '''
